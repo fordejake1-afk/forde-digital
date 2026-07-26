cec97259c51e47e76d6132c9468483c184f82a60
Jake Forde <fordejake1@gmail.com>
2026-07-26 13:01:07 +0100

    Made website new amimations

--- script.js
+++ script.js
@@ -4,20 +4,64 @@ console.log("Forde Digital loaded");
 
 
 // Smooth scrolling
+
 document.querySelectorAll("a").forEach(link => {
 
     link.addEventListener("click", function(e) {
 
         const target = document.querySelector(this.getAttribute("href"));
 
-        if(target) {
+        if(target){
+
             e.preventDefault();
 
             target.scrollIntoView({
-                behavior: "smooth"
+
+                behavior:"smooth"
+
             });
+
         }
 
     });
+
+});
+
+
+
+
+// Scroll reveal animation
+
+const cards = document.querySelectorAll(".card");
+const sections = document.querySelectorAll("section");
+
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+
+});
+
+
+
+cards.forEach(card => {
+
+    observer.observe(card);
+
+});
+
+
+sections.forEach(section => {
+
+    observer.observe(section);
 
 });
